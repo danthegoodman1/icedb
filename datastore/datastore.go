dd95836b293d62613ca2d19387e3a849c688f159
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-11-04 19:28:39 -0400

    Add functions to datastore interface

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -7,6 +7,15 @@ var (
 )
 
 type (
+	temp string
+
 	DataStore interface {
+		// GetColumnFile creates a reader for an entire column file
+		GetColumnFile(table, partID, column string) (temp, error)
+		// GetColumnFileWithOffsets creates a reader for a column file with byte offsets
+		GetColumnFileWithOffsets(table, partID, column string) (temp, error)
+
+		// WriteColumnFile creates a writer for a column file
+		WriteColumnFile(table, partID string, t temp) error
 	}
 )
