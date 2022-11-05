4e4a8b4fa5493f6de713ac7ddddd2f3ac007f718
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-11-05 08:47:15 -0400

    Change column mark to a single granule

--- part/part.go
+++ part/part.go
@@ -4,7 +4,8 @@ import "time"
 
 type (
 	Part struct {
-		ID        string
+		ID string
+		// Alive is whether the part should be considered for queries
 		Alive     bool
 		CreatedAt time.Time
 		RowCount  int64
@@ -14,12 +15,16 @@ type (
 
 	ColumnMark struct {
 		PartID string
-		// GranuleIndexes represents the present granules of a part within a column
-		GranuleIndexes []string
+		// GranuleIndex is the index of the granule within the part
+		GranuleIndex int64
 		// ByteOffsets is an ordered byte offset for the granule indexes
-		ByteOffsets []int64
+		ByteOffsets int64
 		ColumnName  string
-		// RowCount is the number of rows within this column
+		// RowCount is the number of rows within this granule
 		RowCount int64
+		// RowStart is the row number that is the first row in the granule, mapped to the
+		// primary key rows. As such row numbers may be skipped within a column if not all rows
+		// in the part contain this column.
+		RowStart int64
 	}
 )
