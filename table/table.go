8e18942ef77e8704facba081329a364b99553e5a
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-11-04 20:59:53 -0400

    Add row interface

--- table/table.go
+++ table/table.go
@@ -13,4 +13,14 @@ type (
 		OrderingKeyColNames []string
 		OrderingKeyColTypes []types.Type
 	}
+
+	Row struct {
+		// The column number within the part
+		Num int64
+
+		// The list of column names, same order as ColVals
+		ColNames []string
+		// The lsit of column values, same order as ColNums
+		ColVals []any
+	}
 )
