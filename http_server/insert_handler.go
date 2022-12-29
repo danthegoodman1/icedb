69a407d05046ac40971729355e5e633fad8564f1
Dan Goodman <dan@danthegoodman.com>
2022-12-29 16:22:17 -0500

    working test and write parquet data

--- http_server/insert_handler.go
+++ http_server/insert_handler.go
@@ -1,16 +1,84 @@
 package http_server
 
-import "net/http"
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/danthegoodman1/gojsonutils"
+	"github.com/danthegoodman1/icedb/parquet_accumulator"
+	"github.com/rs/zerolog"
+	"github.com/xitongsys/parquet-go/writer"
+	"net/http"
+	"time"
+)
+
+var (
+	ErrNotFlatMap = errors.New("not a flat map")
+)
 
 func (s *HTTPServer) InsertHandler(c *CustomContext) error {
+	ctx, cancel := context.WithTimeout(c.Request().Context(), time.Second*60)
+	defer cancel()
+
+	logger := zerolog.Ctx(ctx)
 
 	// Get namespace to write to
 
 	// Extract rows (flattened) and columns from format (JSON, NDJSON)
+	ndJSONScanner := bufio.NewScanner(c.Request().Body)
+	defer c.Request().Body.Close()
+
+	psa := parquet_accumulator.NewParquetAccumulator()
+	var rawFlatRows []map[string]any
+
+	for ndJSONScanner.Scan() {
+		var raw any
+		jsonMap, ok := raw.(map[string]any)
+		if !ok {
+			return c.String(http.StatusBadRequest, "line was not JSON")
+		}
+		flat, err := gojsonutils.Flatten(jsonMap, nil)
+		if err != nil {
+			return c.InternalError(err, "error flattening JSON map")
+		}
+		flatMap, ok := flat.(map[string]any)
+		if !ok {
+			return c.InternalError(ErrNotFlatMap, fmt.Sprintf("got a non flat map: %+v", flat))
+		}
+
+		psa.WriteRow(flatMap)
+	}
 
 	// Generate parquet schema from columns
+	parquetSchema, err := psa.GetSchemaString()
+	if err != nil {
+		return c.InternalError(err, "error in GetSchemaString")
+	}
 
 	// Convert rows to a parquet file
+	var b bytes.Buffer
+	pw, err := writer.NewJSONWriterFromWriter(parquetSchema, &b, 4)
+	if err != nil {
+		return c.InternalError(err, "error in NewJSONWriterFromWriter")
+	}
+
+	for _, row := range rawFlatRows {
+		bytes, err := json.Marshal(row)
+		if err != nil {
+			return c.InternalError(err, "error in json.Marshal of flat row")
+		}
+		err = pw.Write(bytes)
+		if err != nil {
+			return c.InternalError(err, "error in pw.Write")
+		}
+	}
+	err = pw.WriteStop()
+	if err != nil {
+		return c.InternalError(err, "error in pw.WriteStop")
+	}
 
 	// Write parquet file to S3
 
---
+++ parquet_accumulator/parquet_accumulator_test.go
@@ -0,0 +1,138 @@
+package parquet_accumulator
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/danthegoodman1/gojsonutils"
+	"github.com/xitongsys/parquet-go-source/local"
+	"github.com/xitongsys/parquet-go/reader"
+	"github.com/xitongsys/parquet-go/writer"
+	"os"
+	"testing"
+)
+
+func TestGetSchemaString(t *testing.T) {
+	a := NewParquetAccumulator()
+	a.WriteRow(map[string]any{
+		"colA": "hey",
+	})
+	a.WriteRow(map[string]any{
+		"colB": 1.2,
+	})
+	a.WriteRow(map[string]any{
+		"colC": []any{"hey"},
+	})
+
+	a.WriteRow(map[string]any{
+		"colA": "hey",
+		"colB": 1,
+	})
+
+	a.WriteRow(map[string]any{
+		"colC": []any{"hey"},
+		"colB": 1.2,
+	})
+
+	schemaString, err := a.GetSchemaString()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if schemaString != `{"Tag":"name=parquet_go_root, repetitiontype=REQUIRED","Fields":[{"Tag":"type=BYTE_ARRAY, convertedtype=UTF8, encoding=PLAIN, name=colA, repetitiontype=OPTIONAL"},{"Tag":"type=DOUBLE, name=colB, repetitiontype=OPTIONAL"},{"Tag":"type=LIST, name=colC, repetitiontype=OPTIONAL","Fields":[{"Tag":"type=BYTE_ARRAY, convertedtype=UTF8, encoding=PLAIN, name=Element, repetitiontype=OPTIONAL"}]}]}` {
+		t.Log(schemaString)
+		t.Fatal("got incorrect schema string")
+	}
+
+}
+
+func TestFullCycle(t *testing.T) {
+	jsonMap := map[string]any{
+		"colA": map[string]any{
+			"a": "hey",
+			"b": 2,
+		},
+		"colC": []any{"hey"},
+		"colB": 1.2,
+	}
+	flat, err := gojsonutils.Flatten(jsonMap, nil)
+	if err != nil {
+		t.Fatal("error flattening JSON map")
+	}
+	flatMap, ok := flat.(map[string]any)
+	if !ok {
+		t.Fatal(fmt.Sprintf("got a non flat map: %+v", flat))
+	}
+	psa := NewParquetAccumulator()
+	psa.WriteRow(flatMap)
+
+	parquetSchema, err := psa.GetSchemaString()
+	if err != nil {
+		t.Fatal("error in GetSchemaString")
+	}
+
+	f, err := os.Create("temp.parquet")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(parquetSchema)
+	pw, err := writer.NewJSONWriterFromWriter(parquetSchema, f, 4)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(flatMap)
+
+	b, err := json.Marshal(flatMap)
+	if err != nil {
+		t.Fatal("error in json.Marshal: %w", err)
+	}
+
+	err = pw.Write(string(b))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = pw.WriteStop()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	f.Close()
+
+	fr, err := local.NewLocalFileReader("temp.parquet")
+	if err != nil {
+		t.Fatal("Can't open file", err)
+	}
+
+	pr, err := reader.NewParquetReader(fr, parquetSchema, 4)
+	if err != nil {
+		t.Fatal("Can't create parquet reader", err)
+		return
+	}
+
+	num := int(pr.GetNumRows())
+	t.Log("rows", num)
+	//for i := 0; i < num; i++ {
+	//	stus := make(map[string]any)
+	//	if err = pr.Read(&stus); err != nil {
+	//		t.Fatal("Read error", err)
+	//	}
+	//	t.Log("got", stus)
+	//}
+
+	res, err := pr.ReadByNumber(num)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, item := range res {
+		// item is a struct
+		b, err := json.Marshal(item)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("%+v", string(b))
+	}
+
+	pr.ReadStop()
+	fr.Close()
+}
