c4e31cd8fe38a854d76ac93edd5dd1f4920551eb
Dan Goodman <dan@danthegoodman.com>
2022-12-29 17:19:00 -0500

    working tested

--- http_server/insert_handler.go
+++ http_server/insert_handler.go
@@ -14,7 +14,6 @@ import (
 	"github.com/danthegoodman1/icedb/s3"
 	"github.com/danthegoodman1/icedb/utils"
 	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/rs/zerolog"
 	"github.com/xitongsys/parquet-go/writer"
 	"net/http"
 	"time"
@@ -36,7 +35,7 @@ func (s *HTTPServer) InsertHandler(c *CustomContext) error {
 	ctx, cancel := context.WithTimeout(c.Request().Context(), time.Second*60)
 	defer cancel()
 
-	logger := zerolog.Ctx(ctx)
+	//logger := zerolog.Ctx(ctx)
 
 	start := time.Now()
 	// Get namespace to write to
@@ -50,6 +49,10 @@ func (s *HTTPServer) InsertHandler(c *CustomContext) error {
 
 	for ndJSONScanner.Scan() {
 		var raw any
+		err := json.Unmarshal([]byte(ndJSONScanner.Text()), &raw)
+		if err != nil {
+			return fmt.Errorf("error in json.Unmarshal: %w", err)
+		}
 		jsonMap, ok := raw.(map[string]any)
 		if !ok {
 			return c.String(http.StatusBadRequest, "line was not JSON")
@@ -63,7 +66,12 @@ func (s *HTTPServer) InsertHandler(c *CustomContext) error {
 			return c.InternalError(ErrNotFlatMap, fmt.Sprintf("got a non flat map: %+v", flat))
 		}
 
+		rawFlatRows = append(rawFlatRows, flatMap)
 		psa.WriteRow(flatMap)
+	}
+
+	if len(rawFlatRows) == 0 {
+		return c.String(http.StatusBadRequest, "no rows found")
 	}
 
 	// Generate parquet schema from columns
@@ -94,6 +102,8 @@ func (s *HTTPServer) InsertHandler(c *CustomContext) error {
 		return c.InternalError(err, "error in pw.WriteStop")
 	}
 
+	byteLen := b.Len()
+
 	// Write parquet file to S3
 	// TODO: Based on partition scheme
 	fileName := fmt.Sprintf("p/%s.parquet", utils.GenKSortedID(""))
@@ -109,7 +119,7 @@ func (s *HTTPServer) InsertHandler(c *CustomContext) error {
 			Namespace: "tempns",
 			Enabled:   true,
 			Path:      fileName,
-			Bytes:     int64(b.Len()),
+			Bytes:     int64(byteLen),
 			Rows:      int64(len(rawFlatRows)),
 			Columns:   psa.GetColumns(),
 		})
@@ -119,7 +129,7 @@ func (s *HTTPServer) InsertHandler(c *CustomContext) error {
 	// Respond with metrics
 	stats := InsertStats{
 		NumRows:      int64(len(rawFlatRows)),
-		BytesWritten: int64(b.Len()),
+		BytesWritten: int64(byteLen),
 		TimeNS:       end.Nanoseconds(),
 	}
 
