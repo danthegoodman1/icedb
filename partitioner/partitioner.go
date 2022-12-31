8e0d9e7ae9360226d4e9c0b84b91ac7c89ea6429
Dan Goodman <dan@danthegoodman.com>
2022-12-31 12:52:56 -0500

    Add many more time functions

--- partitioner/partitioner.go
+++ partitioner/partitioner.go
@@ -29,38 +29,52 @@ var (
 
 func RegisterFunctions() {
 	Functions["toDay"] = func(row map[string]any, args []string) (string, error) {
-		if len(args) == 0 {
-			return "", ErrMissingArgs
-		}
-
-		var t time.Time
-
-		key := args[0]
-
-		if key == "now()" {
-			t = time.Now()
-		} else {
-			value, exists := row[key]
-			if !exists {
-				return "", ErrMissingColumns
-			}
-
-			var err error
-			if valString, isStr := value.(string); isStr {
-				// We have a datetime like YYYY-MM-DDTHH:mm:ss.sssZ
-				t, err = time.Parse("2006-01-02T15:04:05.000Z", valString)
-				if err != nil {
-					return "", fmt.Errorf("error in time.Parse for string: %w", err)
-				}
-			} else if valFloat, isFloat := value.(float64); isFloat {
-				// We have a float as an int
-				t = time.UnixMilli(int64(valFloat))
-			} else {
-				return "", ErrInvalidColumnType
-			}
+		t, err := parseTimeFuncfunc(row, args)
+		if err != nil {
+			return "", fmt.Errorf("error in parseTimeFunc: %w", err)
 		}
 
 		return fmt.Sprint(t.Day()), nil
+	}
+	Functions["toMonth"] = func(row map[string]any, args []string) (string, error) {
+		t, err := parseTimeFuncfunc(row, args)
+		if err != nil {
+			return "", fmt.Errorf("error in parseTimeFunc: %w", err)
+		}
+
+		return fmt.Sprint(t.Month()), nil
+	}
+	Functions["toYear"] = func(row map[string]any, args []string) (string, error) {
+		t, err := parseTimeFuncfunc(row, args)
+		if err != nil {
+			return "", fmt.Errorf("error in parseTimeFunc: %w", err)
+		}
+
+		return fmt.Sprint(t.Year()), nil
+	}
+	Functions["toYearDay"] = func(row map[string]any, args []string) (string, error) {
+		t, err := parseTimeFuncfunc(row, args)
+		if err != nil {
+			return "", fmt.Errorf("error in parseTimeFunc: %w", err)
+		}
+
+		return fmt.Sprint(t.YearDay()), nil
+	}
+	Functions["toYearWeek"] = func(row map[string]any, args []string) (string, error) {
+		t, err := parseTimeFuncfunc(row, args)
+		if err != nil {
+			return "", fmt.Errorf("error in parseTimeFunc: %w", err)
+		}
+
+		return fmt.Sprint(t.ISOWeek()), nil
+	}
+	Functions["toWeekDay"] = func(row map[string]any, args []string) (string, error) {
+		t, err := parseTimeFuncfunc(row, args)
+		if err != nil {
+			return "", fmt.Errorf("error in parseTimeFunc: %w", err)
+		}
+
+		return fmt.Sprint(t.Weekday()), nil
 	}
 }
 
@@ -79,4 +93,40 @@ func GetRowPartition(row map[string]any, partitioners []PartitionPlan) (string, 
 		finalParts = append(finalParts, fmt.Sprintf("%s=%s", partFunc.As, s))
 	}
 	return strings.Join(finalParts, "/"), nil
+}
+
+func parseTimeFuncfunc(row map[string]any, args []string) (t time.Time, err error) {
+	if len(args) == 0 {
+		err = ErrMissingArgs
+		return
+	}
+
+	key := args[0]
+
+	if key == "now()" {
+		t = time.Now()
+	} else {
+		value, exists := row[key]
+		if !exists {
+			err = ErrMissingColumns
+			return
+		}
+
+		var err error
+		if valString, isStr := value.(string); isStr {
+			// We have a datetime like YYYY-MM-DDTHH:mm:ss.sssZ
+			t, err = time.Parse("2006-01-02T15:04:05.000Z", valString)
+			if err != nil {
+				err = fmt.Errorf("error in time.Parse for string: %w", err)
+				return
+			}
+		} else if valFloat, isFloat := value.(float64); isFloat {
+			// We have a float as an int
+			t = time.UnixMilli(int64(valFloat))
+		} else {
+			err = ErrInvalidColumnType
+			return
+		}
+	}
+	return
 }
