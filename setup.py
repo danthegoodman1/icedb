a291dfae702f923269cc393a73c0bd2fdcf5018c
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-06-13 00:39:26 +0000

    fix for real

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = '0.0.47'
+VERSION = '0.0.48'
 DESCRIPTION = 'IceDB'
 LONG_DESCRIPTION = 'IceDB in-process serverless OLAP powered by DuckDB'
 
