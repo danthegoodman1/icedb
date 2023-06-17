934b0f9940e4e5e91a87669514edd08c22531c04
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-06-17 20:49:15 +0000

    manual transaction management

--- icedb/icedb.py
+++ icedb/icedb.py
@@ -88,20 +88,20 @@ class IceDB:
 
         if create_table:
             # trick for using autocommit
-            with self.conn:
-                with self.conn.cursor() as cursor:
-                    # make sure the table exists
-                    cursor.execute('''
-                        create table if not exists known_files (
-                            partition TEXT NOT NULL,
-                            filename TEXT NOT NULL,
-                            filesize INT8 NOT NULL,
-                            active BOOLEAN NOT NULL DEFAULT TRUE,
-                            _created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-                            _updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-                            PRIMARY KEY(active, partition, filename)
-                        )
-                    ''')
+            with self.conn.cursor() as cursor:
+                # make sure the table exists
+                cursor.execute('''
+                    create table if not exists known_files (
+                        partition TEXT NOT NULL,
+                        filename TEXT NOT NULL,
+                        filesize INT8 NOT NULL,
+                        active BOOLEAN NOT NULL DEFAULT TRUE,
+                        _created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                        _updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                        PRIMARY KEY(active, partition, filename)
+                    )
+                ''')
+                self.conn.commit()
     def close(self):
         self.conn.close()
 
@@ -153,11 +153,11 @@ class IceDB:
             fileSize = obj['ContentLength']
 
             # insert into meta store
-            with self.conn:
-                with self.conn.cursor() as cursor:
-                    cursor.execute('''
-                        insert into known_files (filename, filesize, partition)  VALUES ('{}', {}, '{}')
-                    '''.format(filename, fileSize, part))
+            with self.conn.cursor() as cursor:
+                cursor.execute('''
+                    insert into known_files (filename, filesize, partition)  VALUES ('{}', {}, '{}')
+                '''.format(filename, fileSize, part))
+                self.conn.commit()
 
         return final_files
 
@@ -172,8 +172,8 @@ class IceDB:
         curid = "a"+str(uuid4()).replace("-", "")
         buf = []
         fsum = 0
-        with self.conn:
-            with self.conn.cursor() as mycur:
+        with self.conn.cursor() as mycur:
+            try:
                 if self.set_isolation:
                     # don't need serializable isolation here, just need a snapshot
                     # if the files change between the next transaction, then they will be omitted from the first query selecting them
@@ -225,91 +225,97 @@ class IceDB:
 
                         buf.append(row)
                         fsum += row[2]
+            except:
+                self.conn.rollback()
 
-            # select the files for update to make sure they are all still active, anything not active we drop (from colliding merges)
-            # if there is only 1 file, then we have nothing to merge
-            if len(buf) > 1:
-                partition = buf[0][0]
-                # merge these files, update DB
-                print('I have files for merging! going to lock them now')
-                with self.conn.cursor() as mergecur:
-                    # lock the files up
-                    if self.set_isolation:
-                        # now we need serializable isolation to protect against concurrent merges
-                        mergecur.execute("set transaction isolation level serializable")
+        with self.conn.cursor() as mycur:
+            try:
+                # select the files for update to make sure they are all still active, anything not active we drop (from colliding merges)
+                # if there is only 1 file, then we have nothing to merge
+                if len(buf) > 1:
+                    partition = buf[0][0]
+                    # merge these files, update DB
+                    print('I have files for merging! going to lock them now')
+                    with self.conn.cursor() as mergecur:
+                        # lock the files up
+                        if self.set_isolation:
+                            # now we need serializable isolation to protect against concurrent merges
+                            mergecur.execute("set transaction isolation level serializable")
 
-                    q = '''
-                        select filename
-                        from known_files
-                        where active = true
-                        and partition = '{}'
-                        and filename in ({})
-                        for update
-                    '''.format(partition, ','.join(list(map(lambda x: "'{}'".format(x[1]), buf))))
-                    mergecur.execute(q)
-                    rows = mergecur.fetchall()
-                    actual_files = list(map(lambda x: x[0], rows))
-                    if len(actual_files) == 0:
-                        print('no actual files during merge, were there competing merges? I am exiting.')
-                        return 0
-                    if len(actual_files) == 1:
-                        print('only got a single file when locking files for merging, were there competing merges? I am exiting.')
-                        return 0
-                    new_f_name = '{}.parquet'.format(str(uuid4()))
-                    new_f_path = partition + "/" + new_f_name
-                    
-                    # copy the files in S3
-                    q = '''
-                    COPY (
-                        {}
-                    ) TO '{}'
-                    '''.format(
-                        ('''
-                        select *
-                        from source_files
-                        ''' if custom_merge_query is None else custom_merge_query).replace("source_files", "read_parquet(?, hive_partitioning=1)"),
-                        's3://{}/{}'.format(self.s3bucket, new_f_path)
-                    )
+                        q = '''
+                            select filename
+                            from known_files
+                            where active = true
+                            and partition = '{}'
+                            and filename in ({})
+                            for update
+                        '''.format(partition, ','.join(list(map(lambda x: "'{}'".format(x[1]), buf))))
+                        mergecur.execute(q)
+                        rows = mergecur.fetchall()
+                        actual_files = list(map(lambda x: x[0], rows))
+                        if len(actual_files) == 0:
+                            print('no actual files during merge, were there competing merges? I am exiting.')
+                            return 0
+                        if len(actual_files) == 1:
+                            print('only got a single file when locking files for merging, were there competing merges? I am exiting.')
+                            return 0
+                        new_f_name = '{}.parquet'.format(str(uuid4()))
+                        new_f_path = partition + "/" + new_f_name
+                        
+                        # copy the files in S3
+                        q = '''
+                        COPY (
+                            {}
+                        ) TO '{}'
+                        '''.format(
+                            ('''
+                            select *
+                            from source_files
+                            ''' if custom_merge_query is None else custom_merge_query).replace("source_files", "read_parquet(?, hive_partitioning=1)"),
+                            's3://{}/{}'.format(self.s3bucket, new_f_path)
+                        )
 
-                    self.ddb.execute(q, [
-                        list(map(lambda x: "s3://{}/{}/{}".format(self.s3bucket, partition, x), actual_files))
-                    ])
+                        self.ddb.execute(q, [
+                            list(map(lambda x: "s3://{}/{}/{}".format(self.s3bucket, partition, x), actual_files))
+                        ])
 
-                    # get the new file size
-                    obj = self.s3.head_object(
-                        Bucket=self.s3bucket,
-                        Key=new_f_path
-                    )
-                    new_f_size = obj['ContentLength']
+                        # get the new file size
+                        obj = self.s3.head_object(
+                            Bucket=self.s3bucket,
+                            Key=new_f_path
+                        )
+                        new_f_size = obj['ContentLength']
 
-                    # insert the new file
-                    mergecur.execute('''
-                        insert into known_files (filename, filesize, partition)  VALUES ('{}', {}, '{}')
-                    '''.format(new_f_name, new_f_size, partition))
+                        # insert the new file
+                        mergecur.execute('''
+                            insert into known_files (filename, filesize, partition)  VALUES ('{}', {}, '{}')
+                        '''.format(new_f_name, new_f_size, partition))
 
-                    # update the old files
-                    q = '''
-                        update known_files
-                        set active = false
-                        and _updated = NOW()
-                        where active = true
-                        and partition = '{}'
-                        and filename in ({})
-                    '''.format(partition, ','.join(list(map(lambda x: "'{}'".format(x), actual_files))))
-                    mergecur.execute(q)
-                    return len(actual_files)
+                        # update the old files
+                        q = '''
+                            update known_files
+                            set active = false
+                            and _updated = NOW()
+                            where active = true
+                            and partition = '{}'
+                            and filename in ({})
+                        '''.format(partition, ','.join(list(map(lambda x: "'{}'".format(x), actual_files))))
+                        mergecur.execute(q)
+                        return len(actual_files)
+            except:
+                self.conn.rollback()
         return 0
 
     def get_files(self, gte_part: str, lte_part: str) -> List[str]:
-        with self.conn:
-            with self.conn.cursor() as mycur:
-                mycur.execute('''
-                select partition, filename
-                from known_files
-                where active = true
-                AND partition >= %s
-                AND partition <= %s
-                ''', (gte_part, lte_part))
-                rows = mycur.fetchall()
-                print('get_files got {} files'.format(len(rows)))
-                return list(map(lambda x: 's3://{}/{}/{}'.format(self.s3bucket, x[0], x[1]), rows))
+        with self.conn.cursor() as mycur:
+            mycur.execute('''
+            select partition, filename
+            from known_files
+            where active = true
+            AND partition >= %s
+            AND partition <= %s
+            ''', (gte_part, lte_part))
+            rows = mycur.fetchall()
+            self.conn.commit()
+            print('get_files got {} files'.format(len(rows)))
+            return list(map(lambda x: 's3://{}/{}/{}'.format(self.s3bucket, x[0], x[1]), rows))
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.54'
+VERSION = '0.0.55'
 DESCRIPTION = 'IceDB'
 LONG_DESCRIPTION = 'IceDB in-process serverless OLAP powered by DuckDB'
 
