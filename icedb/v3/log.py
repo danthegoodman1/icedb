96b6476d1b59f365cdeab8ac68e7399c35ef9fe5
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-08-04 17:46:11 +0000

    file append work

--- icedb/v3/log.py
+++ icedb/v3/log.py
@@ -1,4 +1,7 @@
 import json
+import boto3
+import botocore
+from ksuid import ksuid
 
 
 class SchemaConflictException(Exception):
@@ -14,7 +17,7 @@ class SchemaConflictException(Exception):
 
 class Schema:
     """
-    Accumulated schema. It is safe to pass in columns and types redundantly in the constructor.
+    Accumulated schema. It is safe to pass in columns and types redundantly in the constructor. Can raise a `SchemaConflictException` if conflicting types are passed in.
     """
     d = {}
 
@@ -90,25 +93,25 @@ class FileTombstone:
 
 class LogMetadata:
     version: int
-    schemaStartLine: int
-    fileStartLine: int
-    tombstoneStartLine: int | None
+    schemaLineIndex: int
+    fileLineIndex: int
+    tombstoneLineIndex: int | None
 
-    def __init__(self, version: int, schemaStartLine: int, fileStartLine: int, tombstoneStartLine: int | None):
+    def __init__(self, version: int, schemaLineIndex: int, fileLineIndex: int, tombstoneLineIndex: int | None):
         self.version = version
-        self.schemaStartLine = schemaStartLine
-        self.fileStartLine = fileStartLine
-        self.tombstoneStartLine = tombstoneStartLine
+        self.schemaLineIndex = schemaLineIndex
+        self.fileLineIndex = fileLineIndex
+        self.tombstoneLineIndex = tombstoneLineIndex
 
     def toJSON(self) -> str:
         d = {
             "v": self.version,
-            "sch": self.schemaStartLine,
-            "f": self.fileStartLine
+            "sch": self.schemaLineIndex,
+            "f": self.fileLineIndex
         }
 
-        if self.tombstoneStartLine is not None:
-            d["tmb"] = self.tombstoneStartLine
+        if self.tombstoneLineIndex is not None:
+            d["tmb"] = self.tombstoneLineIndex
 
         return json.dumps(d)
 
@@ -116,17 +119,52 @@ class LogMetadata:
 class IceLog:
 
     path: str
+    session: any
+    s3: any
+    s3bucket: str
 
     def __init__(
         self,
-        path: str
+        path: str,
+        s3bucket: str,
+        s3region: str,
+        s3endpoint: str,
+        s3accesskey: str,
+        s3secretkey: str
     ):
         self.path = path
+        self.session = boto3.session.Session()
+        self.s3 = self.session.client('s3',
+            config=botocore.config.Config(s3={'addressing_style': 'path'}),
+            region_name=s3region,
+            endpoint_url=s3endpoint,
+            aws_access_key_id=s3accesskey,
+            aws_secret_access_key=s3secretkey
+        )
+        self.s3bucket = s3bucket
 
-    def readAtMaxTime(timestamp: int):
+    def readAtMaxTime(self, timestamp: int):
         pass
 
-    def append(version: int, schema: Schema, files: list[FileMarker], tombstones: list[FileTombstone]):
+    def append(self, version: int, schema: Schema, files: list[FileMarker], tombstones: list[FileTombstone] | None):
         """
-        Creates a new log file in S3
+        Creates a new log file in S3, in the order of version, schema, tombstones?, files
         """
+        logFileLines: list[str] = []
+        meta = LogMetadata(version, 1, 2 if tombstones == None else 2+len(tombstones), None if tombstones == None else 2)
+        logFileLines.append(meta.toJSON())
+        logFileLines.append(schema.toJSON())
+        if tombstones != None:
+            for tmb in tombstones:
+                logFileLines.append(tmb.toJSON())
+        for fileMarker in files:
+            logFileLines.append(fileMarker)
+
+        fileID = ksuid().__str__()
+
+        # Upload the file to S3
+        self.s3.put_object(
+            Body=bytes('\n'.join(logFileLines)),
+            Bucket=self.s3bucket,
+            Key=f"{self.path}/${fileID}.jsonl"
+        )
