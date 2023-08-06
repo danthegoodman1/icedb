c4e4129d26bdfdf27b7bbc1ba8edf9f4ee1dcb6a
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-08-06 16:34:51 -0400

    fix import

--- icedb/__init__.py
+++ icedb/__init__.py
@@ -1,5 +1,5 @@
-from icedb.log import (
+from .log import (
     IceLogIO, Schema, LogMetadata, LogTombstone, NoLogFilesException, FileMarker, S3Client,
     LogMetadataFromJSON, FileMarkerFromJSON, LogTombstoneFromJSON, SchemaConflictException, get_log_file_info
 )
-from icedb.icedb import IceDBv3, PartitionFunctionType, FormatRowType
+from .icedb import IceDBv3, PartitionFunctionType, FormatRowType
