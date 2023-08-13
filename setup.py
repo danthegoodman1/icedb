e2dd4efff1131733c02bfc9596330d6312dbe414
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-08-13 10:18:16 -0400

    bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.84'
+VERSION = '0.3.0'
 DESCRIPTION = 'IceDB'
 LONG_DESCRIPTION = 'IceDB in-process serverless OLAP powered by DuckDB'
 
