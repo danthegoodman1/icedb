653e60a0f635b0ae350a0e897bbf08c90aa12117
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-09-10 09:39:04 -0400

    fix output

--- perf_tests/chicago_taxis/download.sh
+++ perf_tests/chicago_taxis/download.sh
@@ -1,2 +1,2 @@
 echo downloading data
-time wget https://data.cityofchicago.org/api/views/wrvz-psew/rows.csv?accessType=DOWNLOAD -o chicago_taxis.csv
+time wget https://data.cityofchicago.org/api/views/wrvz-psew/rows.csv?accessType=DOWNLOAD -O chicago_taxis.csv
